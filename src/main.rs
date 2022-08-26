57c2b9231f3723f8480f7c7c14fa21c6243b8c94
Krafi <jakub.havlicek5@gmail.com>
2022-08-26 11:18:28 +0200

    Refactor main

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use std::{
     cell::Cell,
     fs::File,
     io::{BufRead, BufReader},
-    path::PathBuf,
+    path::{Path, PathBuf},
     rc::Rc,
 };
 
@@ -30,19 +30,24 @@ fn main() {
         .parse::<PathBuf>()
         .unwrap();
 
-    // Only clean the output folder if the path is relative to avoid accidentaly deleting important
-    // files.
-    if output.exists() && output.is_relative() {
-        for entry in std::fs::read_dir(&output).expect("Failed to open output directory") {
-            if let Ok(entry) = entry {
-                // Delete only the thumbnail files
-                if entry.file_name().to_string_lossy().contains("thumbnails-") {
-                    if let Err(err) = std::fs::remove_file(entry.path()) {
-                        eprintln!("Error removing file: {}", err);
+    // Prepare the output directory
+    if output.exists() {
+        // Only clean the output folder if the path is relative to avoid accidentaly deleting important
+        // files.
+        if output.is_relative() {
+            for entry in std::fs::read_dir(&output).expect("Failed to open output directory") {
+                if let Ok(entry) = entry {
+                    // Delete only the thumbnail files
+                    if entry.file_name().to_string_lossy().contains("thumbnails-") {
+                        if let Err(err) = std::fs::remove_file(entry.path()) {
+                            eprintln!("Error removing file: {}", err);
+                        }
                     }
                 }
             }
         }
+    } else {
+        std::fs::create_dir(&output).expect("Failed to create output directory");
     }
 
     let logger = Rc::new(Cell::new(Some(agent::Logger::new(
@@ -50,67 +55,24 @@ fn main() {
         EXPERIMENTS * TRIALS * SHAPE.iter().product::<u32>() / GRAPH_POINTS,
     ))));
 
-    let videos = std::fs::read_dir(input)
-        .expect("Failed to read dir")
-        .map(|video| {
-            let path = video.unwrap().path();
-            let ctrs = path.join("ctrs.txt");
-
-            // Parse the thumbnail data from the csv file
-            let thumbs = BufReader::new(File::open(ctrs).expect("Failed to read input"))
-                .lines()
-                .map(|s| {
-                    let s = s.expect("Failed to read line");
-                    let sub = s.split(',').collect::<Vec<_>>();
-
-                    Thumbnail::new(sub[0].parse().unwrap(), sub[1].parse().unwrap())
-                })
-                .collect::<Vec<_>>();
-
+    let videos = load_data(input.as_ref())
+        .map(|(path, thumbs)| {
             let agent = ThompsonSampler::new(thumbs.len());
-
             Video::new(thumbs, RegretLogger::new(agent, logger.clone()), path)
         })
-        .collect::<Vec<_>>();
+        .collect();
 
     let mat_len = SHAPE.iter().product::<u32>() as usize;
-    let clickability = (1..=mat_len)
-        .map(|i| 1. - (i as f32 / mat_len as f32).sqrt())
+    let clickability = (0..mat_len)
+        .map(|i| 1. - 0.9 * (i as f32 / mat_len as f32).sqrt())
         .collect();
     let mut experiment = Experiment::new(videos, SHAPE, TRIALS, clickability, TEST_PROB, SEED);
 
     for e in 0..EXPERIMENTS {
         experiment.run();
-        let (impressions, _) = experiment.generate_impressions();
-
-        let sample_thumb = impressions[0].0;
-        let sample_thumb =
-            image::open(experiment.videos()[sample_thumb.video()].thumb_path(sample_thumb))
-                .expect("Failed to read image");
-        let (img_width, img_height) = sample_thumb.dimensions();
-        let [width, height] = SHAPE;
-
-        let mut image = image::RgbImage::new(width * img_width, height * img_height);
-
-        for y in 0..height {
-            for x in 0..width {
-                let n = y * width + x;
-                let id = impressions[n as usize].0;
-                let path = experiment.videos()[id.video()].thumb_path(id);
-                let thumb = image::open(path).expect("Failed to read image").into_rgb8();
-                image::imageops::overlay(
-                    &mut image,
-                    &thumb,
-                    (x * img_width) as i64,
-                    (y * img_height) as i64,
-                );
-            }
-        }
-
-        image
-            .save(output.join(format!("thumbnails-{:02}.png", e)))
+        let img = rotator_snapshot(&mut experiment);
+        img.save(output.join(format!("thumbnails-{:02}.png", e)))
             .expect("Failed to write thumbnails");
-
         println!("Finished experiment {}", e);
     }
     let mut logger = logger.take().expect("Logfile is in use");
@@ -132,7 +94,7 @@ fn main() {
         .map(|click| click.ratio() / max);
 
     println!("Clickability matrix estimate:");
-    println!("N | real | estimate");
+    println!(" N |   real  | estimate");
     for (i, (real, estimate)) in experiment
         .clickability()
         .into_iter()
@@ -159,4 +121,55 @@ fn main() {
     }
 
     println!("Done!");
+}
+
+fn load_data(input: &Path) -> impl Iterator<Item = (PathBuf, Vec<Thumbnail>)> {
+    std::fs::read_dir(input)
+        .expect("Failed to read dir")
+        .map(|video| {
+            let path = video.unwrap().path();
+            let ctrs = path.join("ctrs.txt");
+
+            // Parse the thumbnail data from the csv file
+            let thumbs = BufReader::new(File::open(ctrs).expect("Failed to read input"))
+                .lines()
+                .map(|s| {
+                    let s = s.expect("Failed to read line");
+                    let sub = s.split(',').collect::<Vec<_>>();
+
+                    Thumbnail::new(sub[0].parse().unwrap(), sub[1].parse().unwrap())
+                })
+                .collect::<Vec<_>>();
+            (path, thumbs)
+        })
+}
+
+fn rotator_snapshot(experiment: &mut Experiment) -> image::RgbImage {
+    let (impressions, _) = experiment.generate_impressions();
+
+    let sample_thumb = impressions[0].0;
+    let sample_thumb =
+        image::open(experiment.videos()[sample_thumb.video()].thumb_path(sample_thumb))
+            .expect("Failed to read image");
+    let (img_width, img_height) = sample_thumb.dimensions();
+    let [width, height] = SHAPE;
+
+    let mut image = image::RgbImage::new(width * img_width, height * img_height);
+
+    for y in 0..height {
+        for x in 0..width {
+            let n = y * width + x;
+            let id = impressions[n as usize].0;
+            let path = experiment.videos()[id.video()].thumb_path(id);
+            let thumb = image::open(path).expect("Failed to read image").into_rgb8();
+            image::imageops::overlay(
+                &mut image,
+                &thumb,
+                (x * img_width) as i64,
+                (y * img_height) as i64,
+            );
+        }
+    }
+
+    image
 }
